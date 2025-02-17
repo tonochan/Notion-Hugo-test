88d3b8372037f298fe20c6c5a555839a33816a9d
tonochan <shuntaro@lightmarks.co.jp>
2025-02-17 22:36:08 +0900

    Update notion-hugo.config.ts

--- notion-hugo.config.ts
+++ notion-hugo.config.ts
@@ -4,7 +4,7 @@ const userConfig: UserConfig = {
     base_url: "https://notion-hugo.pages.dev",
     mount: {
         manual: false,
-        page_url: 'https://pcloud.notion.site/Notion-DoIt-04bcc51cfe4c49938229c35e4f0a6fb6',
+        page_url: 'https://www.notion.so/tonochan/Notion-Hugo-19d3f357b6a8807299ddfdd1d817d90f?pvs=4',
         pages: [
             // {
             //     page_id: '<page_id>',
